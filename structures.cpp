bde7e36ff8e4e438ef66342503643e912d5082e2
mpritam17 <mpritam0017@gmail.com>
2025-06-29 13:28:58 +0530

    made a few Changes

--- structures.cpp
+++ structures.cpp
@@ -1,6 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// There can be 12 cube operations : 
+// Namely - U, U', D, D', L, L', R, R', F, F', B, B'
+// and for each face, I have to define 10 operations
+
 class face {
     private :
         vector <vector <char>> colors;
@@ -31,12 +35,16 @@ class face {
             colors = temp;
         }
 
-        int diff(int) {
+        int diff() {
             int d = 0;
+            map <char, int> m;
             for (vector <char> &i: colors) {
                 for (char &c : i) {
-                    if (c != colors[1][1]) d++;
+                    m[c]++;
                 }
+            }
+            for (auto &p : m) {
+                if (p.first != colors[1][1]) d++;
             }
             return d;
         }
@@ -47,11 +55,25 @@ class face {
 class cube {
     private:
         vector <face> a; // front, left, right, up, bottom, back;
+        string seq;
+        int cost = 0;
     public:
-        cube(vector <vector <vector<char>>> c) {
+        cube(vector <vector <vector<char>>> c, string s) : seq(s) {
             for (int i = 0; i < 6; ++i) {
                 a[i] = face(c[i]);
             }
         }
-
+        void calculate_cost() {
+            cost = seq.length();
+            for (face &f : a) {
+                cost += f.diff();
+            }
+            cost -= 6;
+        }
+        int get_cost() {
+            return cost;
+        }
+        string get_seq() {
+            return seq;
+        }
 };
